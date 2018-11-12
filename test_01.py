388e089ed82d05650122d7797b9d4c027445a7c5
wangchunli0411 <13146805032@163.com>
2018-11-12 16:13:32 +0800

    w

---
+++ test_01.py
@@ -0,0 +1,8 @@
+class test:
+
+    def test_01(self):
+        assert 1
+    def test_02(self):
+        assert 1
+    def test_03(self):
+        assert 0
--- text_01.py
+++
@@ -1,8 +0,0 @@
-class a:
-
-    def text_01(self):
-        assert 1
-    def text_02(self):
-        assert 1
-    def text_03(self):
-        assert 0
